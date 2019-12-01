3e912ee305ad20e9517c7afa3bf9b60d07ef9186
Scott Greenlay <scott@greenlay.net>
2019-11-30 17:19:43 -0800

    Reset in preparation for AOC 2019 - Day 1

--- src/day01.rs
+++ src/day01.rs
@@ -4,8 +4,6 @@ use std::io;
 
 use std::fs;
 
-use std::collections::HashSet;
-
 fn read_inputs(filename: String) -> io::Result<Vec<i32>> {
     let file_in = fs::File::open(filename)?;
     let file_reader = io::BufReader::new(file_in);
@@ -15,30 +13,8 @@ fn read_inputs(filename: String) -> io::Result<Vec<i32>> {
 }
 
 pub fn run() {
-    let inputs = read_inputs("data/day01.txt".to_string())
+    let _inputs = read_inputs("data/day01.txt".to_string())
         .expect("Can't read file");
     
-    // Part 1
-    let mut sum = 0;
-    for input in &inputs {
-        sum += input;
-    }
-    println!("Sum is: {}", sum);
-
-    // Part 2
-    let mut sum = 0;
-    let mut sums = HashSet::new();
-    let mut found = false;
-
-    while !found {
-        for input in &inputs {
-            sum += input;
-            if sums.contains(&sum) {
-                println!("First duplicate sum is: {}", sum);
-                found = true;
-                break;
-            }
-            sums.insert(sum);
-        }   
-    }
+    // TODO
 }
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,6 @@
 
-
-#[macro_use]
-extern crate lazy_static;
-
-mod day04;
-use day04::run;
+mod day01;
+use day01::run;
 
 fn main() {
     run();
