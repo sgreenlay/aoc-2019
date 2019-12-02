5e9f25f5ae0b1dfe2df0d4026be78b92a18a4251
Scott Greenlay <scott@greenlay.net>
2019-12-01 22:17:15 -0800

    AOC 2019 - Day 2 (clean-up)

--- src/day02.rs
+++ src/day02.rs
@@ -1,65 +1,77 @@
 
 use std::fs;
 
+fn load_program(filename : String) -> Vec<usize> {
+    let input = fs::read_to_string(filename)
+        .expect("Can't read file");
+
+    let values: Vec<&str> = input.split(',').collect();
+    let program: Vec<usize> = values.iter().map(|x| x.parse::<i32>().unwrap() as usize).collect();
+
+    program
+}
+
 fn run_program(program : &Vec<usize>, noun : usize, verb : usize) -> usize {
     let mut output : Vec<usize> = program.clone();
 
     output[1] = noun;
     output[2] = verb;
 
-    let mut pc = 0;
+    let mut ip = 0;
     loop {
-        match output[pc] {
+        let instruction_size;
+        match output[ip] {
             1 => {
                 // Opcode 1 adds together numbers read from two positions 
-                // and stores the result in a third position. The three 
-                // integers immediately after the opcode tell you these 
-                // three positions - the first two indicate the positions 
-                // from which you should read the input values, and the 
-                // third indicates the position at which the output 
+                // and stores the result in a third position.
+                
+                // The three integers immediately after the opcode tell 
+                // you these three positions - the first two indicate the 
+                // positions from which you should read the input values, 
+                // and the third indicates the position at which the output 
                 // should be stored.
 
-                let a = output[pc + 1];
-                let b = output[pc + 2];
-                let c = output[pc + 3];
+                let load1 = output[ip + 1];
+                let load2 = output[ip + 2];
+                let store = output[ip + 3];
 
-                output[c] = output[a] + output[b];
+                output[store] = output[load1] + output[load2];
+
+                instruction_size = 4;
             }
             2 => {
                 // Opcode 2 works exactly like opcode 1, except it multiplies 
-                // the two inputs instead of adding them. Again, the three 
-                // integers after the opcode indicate where the inputs and 
-                // outputs are, not their values.
+                // the two inputs instead of adding them.
 
-                let a = output[pc + 1];
-                let b = output[pc + 2];
-                let c = output[pc + 3];
+                let load1 = output[ip + 1];
+                let load2 = output[ip + 2];
+                let store = output[ip + 3];
 
-                output[c] = output[a] * output[b];
+                output[store] = output[load1] * output[load2];
+
+                instruction_size = 4;
             }
             99 => {
-                // 99 means that the program is finished and should immediately 
-                // halt.
+                // Opcode 99 means that the program is finished and should 
+                // immediately halt.
                 break;
             }
             _ => {
                 // Encountering an unknown opcode means something went wrong.
+                panic!("Unknown opcode")
             }
         }
-        // Once you're done processing an opcode, move to the next one by 
-        // stepping forward 4 positions.
-        pc += 4;
+        // After an instruction finishes, the instruction pointer increases by 
+        // the number of values in the instruction.
+        ip += instruction_size;
     }
 
+    // Once the program has halted, its output is available at address 0.
     output[0]
 }
 
 pub fn run() {
-    let input = fs::read_to_string("data/day02.txt")
-        .expect("Can't read file");
-
-    let values: Vec<&str> = input.split(',').collect();
-    let program: Vec<usize> = values.iter().map(|x| x.parse::<i32>().unwrap() as usize).collect();
+    let program = load_program("data/day02.txt".to_string());
 
     // Part 1
 
