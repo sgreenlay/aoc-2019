467da0906a906177964134ee71ea0501be7e442e
Scott Greenlay <scott@greenlay.net>
2020-01-04 17:05:06 -0800

    more intcode perf optimizations

--- src/intcode.rs
+++ src/intcode.rs
@@ -39,116 +39,78 @@ impl VirtualMachine {
             self.memory[location]
         }
     }
-    fn next_instruction(&self) -> (u32, Vec<u32>) {
-        let op = self.get_memory(self.ip) as u32;
+    fn get_parameter(&self, i: usize, rw: char) -> i128 {
+        let op = self.get_memory(self.ip);
+        let p = self.get_memory(self.ip + i);
 
-        // Parameter modes are stored in the same value as the instruction's opcode. 
-        // The opcode is a two-digit number based only on the ones and tens digit of 
-        // the value, that is, the opcode is the rightmost two digits of the first 
-        // value in an instruction. Parameter modes are single digits, one per 
-        // parameter, read right-to-left from the opcode: the first parameter's mode 
-        // is in the hundreds digit, the second parameter's mode is in the thousands 
-        // digit, the third parameter's mode is in the ten-thousands digit, and so on.
-        // Any missing modes are 0.
-    
-        let mut i: u32 = 0;
-        let mut modes: Vec<u32> = Vec::new();
-        
-        let mut mult = 1;
-        for idx in 0..5 {
-            let digit = (op / mult) % 10;
-            
-            match idx {
-                0 => {
-                    i = digit;
-                },
-                1 => {
-                    i += digit * 10;
-                },
-                _ => {
-                    modes.push(digit);
-                }
-            }
-
-            mult *= 10;
+        // Any missing modes are 0
+        let m;
+        match i {
+            1 => { m = (op / 100) % 10; }
+            2 => { m = (op / 1000) % 10; }
+            3 => { m = (op / 10000) % 10; }
+            _ => { panic!("Invalid parameter"); }
         }
-    
-        (i, modes)
-    }
-    fn get_parameters(&self, rw: Vec<char>, mode: Vec<u32>) -> Vec<i128> {
-        let ret: Vec<i128> = (0..rw.len()).map(|i| {
-            let p = self.get_memory(self.ip + 1 + i);
 
-            // Any missing modes are 0
-            let m;
-            if i < mode.len() {
-                m = mode[i];
-            } else {
-                m = 0;
+        let ret;
+        match m {
+            0 => {
+                // mode 0, position mode, causes the parameter to be interpreted as a position
+
+                let pos = p as usize;
+                match rw {
+                    'r' => {
+                        ret = self.get_memory(pos);
+                    },
+                    'w' => {
+                        ret = pos as i128;
+                    },
+                    _ => {
+                        panic!("Invalid rw mode");
+                    }
+                }                    
             }
-
-            let ret;
-            match m {
-                0 => {
-                    // mode 0, position mode, causes the parameter to be interpreted as a position
-
-                    let pos = p as usize;
-                    match rw[i] {
-                        'r' => {
-                            ret = self.get_memory(pos);
-                        },
-                        'w' => {
-                            ret = pos as i128;
-                        },
-                        _ => {
-                            panic!("Invalid rw mode");
-                        }
-                    }                    
-                }
-                1 => {
-                    // mode 1, immediate mode, causes a parameter to be interpreted as a value
-                    
-                    match rw[i] {
-                        'r' => {
-                            ret = p;
-                        },
-                        _ => {
-                            panic!("Invalid rw mode");
-                        }
+            1 => {
+                // mode 1, immediate mode, causes a parameter to be interpreted as a value
+                
+                match rw {
+                    'r' => {
+                        ret = p;
+                    },
+                    _ => {
+                        panic!("Invalid rw mode");
                     }
                 }
-                2 => {
-                    // mode 2, relative mode, causes a parameter to be interpreted as a position 
-                    // relative to the relative base
+            }
+            2 => {
+                // mode 2, relative mode, causes a parameter to be interpreted as a position 
+                // relative to the relative base
 
-                    let pos = (self.relative_base + p) as usize;
-                    match rw[i] {
-                        'r' => {
-                            ret = self.get_memory(pos);
-                        },
-                        'w' => {
-                            ret = pos as i128;
-                        }
-                        _ => {
-                            panic!("Invalid rw mode");
-                        }
+                let pos = (self.relative_base + p) as usize;
+                match rw {
+                    'r' => {
+                        ret = self.get_memory(pos);
+                    },
+                    'w' => {
+                        ret = pos as i128;
+                    }
+                    _ => {
+                        panic!("Invalid rw mode");
                     }
                 }
-                _ => {
-                    panic!("Invalid mode");
-                }
             }
-            ret
-        }).collect();
-
+            _ => {
+                panic!("Invalid mode");
+            }
+        }
         ret
     }
     pub fn run(&mut self) -> VirtualMachineState {
         let mut ret: Option<VirtualMachineState> = None;
         while ret.is_none() {
-            let (i, mode) = self.next_instruction();
+            let op = (self.get_memory(self.ip) as u32) % 100;
             let mut instruction_size = 0;
-            match i {
+            match op {
                 1 => {
                     // Opcode 1 adds together numbers read from two positions 
                     // and stores the result in a third position.
@@ -159,8 +121,9 @@ impl VirtualMachine {
                     // and the third indicates the position at which the memory 
                     // should be stored.
 
-                    let p: Vec<i128> = self.get_parameters(vec!['r', 'r', 'w'], mode);
-                    self.set_memory(p[2] as usize, p[0] + p[1]);
+                    self.set_memory(self.get_parameter(3, 'w') as usize,
+                        self.get_parameter(1, 'r') +
+                        self.get_parameter(2, 'r'));
 
                     instruction_size = 4;
                 }
@@ -168,8 +131,9 @@ impl VirtualMachine {
                     // Opcode 2 works exactly like opcode 1, except it multiplies 
                     // the two inputs instead of adding them.
 
-                    let p: Vec<i128> = self.get_parameters(vec!['r', 'r', 'w'], mode);
-                    self.set_memory(p[2] as usize, p[0] * p[1]);
+                    self.set_memory(self.get_parameter(3, 'w') as usize,
+                        self.get_parameter(1, 'r') *
+                        self.get_parameter(2, 'r'));
 
                     instruction_size = 4;
                 }
@@ -177,14 +141,12 @@ impl VirtualMachine {
                     // Opcode 3 takes a single integer as input and saves it to the 
                     // position given by its only parameter.
 
-                    let p: Vec<i128> = self.get_parameters(vec!['w'], mode);
-
                     if self.input.is_empty() {
                         ret = Some(VirtualMachineState::WaitForInput)
                     } else {
                         let input = self.input.remove(0);
 
-                        self.set_memory(p[0] as usize, input);
+                        self.set_memory(self.get_parameter(1, 'w') as usize, input);
 
                         instruction_size = 2;
                     }
@@ -192,8 +154,7 @@ impl VirtualMachine {
                 4 => {
                     // Opcode 4 outputs the value of its only parameter.
 
-                    let p: Vec<i128> = self.get_parameters(vec!['r'], mode);
-                    ret = Some(VirtualMachineState::Output(p[0]));
+                    ret = Some(VirtualMachineState::Output(self.get_parameter(1, 'r')));
 
                     instruction_size = 2;
                 }
@@ -202,9 +163,8 @@ impl VirtualMachine {
                     // it sets the instruction pointer to the value from the second 
                     // parameter. Otherwise, it does nothing.
 
-                    let p: Vec<i128> = self.get_parameters(vec!['r', 'r'], mode);
-                    if p[0] != 0 {
-                        self.ip = p[1] as usize;
+                    if self.get_parameter(1, 'r') != 0 {
+                        self.ip = self.get_parameter(2, 'r') as usize;
                     } else {
                         instruction_size = 3;
                     }
@@ -214,9 +174,8 @@ impl VirtualMachine {
                     // sets the instruction pointer to the value from the second 
                     // parameter. Otherwise, it does nothing.
 
-                    let p: Vec<i128> = self.get_parameters(vec!['r', 'r'], mode);
-                    if p[0] == 0 {
-                        self.ip = p[1] as usize;
+                    if self.get_parameter(1, 'r') == 0 {
+                        self.ip = self.get_parameter(2, 'r') as usize;
                     } else {
                         instruction_size = 3;
                     }
@@ -226,11 +185,10 @@ impl VirtualMachine {
                     // second parameter, it stores 1 in the position given by the 
                     // third parameter. Otherwise, it stores 0.
 
-                    let p: Vec<i128> = self.get_parameters(vec!['r', 'r', 'w'], mode);
-                    if p[0] < p[1] {
-                        self.set_memory(p[2] as usize, 1);
+                    if self.get_parameter(1, 'r') < self.get_parameter(2, 'r') {
+                        self.set_memory(self.get_parameter(3, 'w') as usize, 1);
                     } else {
-                        self.set_memory(p[2] as usize, 0);
+                        self.set_memory(self.get_parameter(3, 'w') as usize, 0);
                     }
 
                     instruction_size = 4;
@@ -240,11 +198,10 @@ impl VirtualMachine {
                     // parameter, it stores 1 in the position given by the third 
                     // parameter. Otherwise, it stores 0.
 
-                    let p: Vec<i128> = self.get_parameters(vec!['r', 'r', 'w'], mode);
-                    if p[0] == p[1] {
-                        self.set_memory(p[2] as usize, 1);
+                    if self.get_parameter(1, 'r') == self.get_parameter(2, 'r') {
+                        self.set_memory(self.get_parameter(3, 'w') as usize, 1);
                     } else {
-                        self.set_memory(p[2] as usize, 0);
+                        self.set_memory(self.get_parameter(3, 'w') as usize, 0);
                     }
 
                     instruction_size = 4;
@@ -253,9 +210,7 @@ impl VirtualMachine {
                     // Opcode 9 adjusts the relative base by the value of its only 
                     // parameter.
 
-                    let p: Vec<i128> = self.get_parameters(vec!['r'], mode);
-
-                    let offset = p[0];
+                    let offset = self.get_parameter(1, 'r');
                     self.relative_base += offset;
 
                     instruction_size = 2;
